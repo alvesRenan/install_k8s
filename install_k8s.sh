f2947ab2c173a3c8aeaaa20e497745316a3f6794
alvesRenan <renanblack7@hotmail.com>
2019-10-18 13:57:28 -0300

    updated file

--- install_k8s.sh
+++ install_k8s.sh
@@ -41,7 +41,7 @@ install_kubernetes() {
 
   echo "Instaling Kubeadm, Kubelet and Kubectl"
 
-  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
   echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 
